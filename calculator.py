ab68ba70e7aef6edaa21986f9405af267b435e40
allisonng88 <alli.ng5238@gmail.com>
2025-04-14 09:35:37 -0400

    added functions and try/except blocks

--- calculator.py
+++ calculator.py
@@ -21,12 +21,18 @@ try:
     a = int(input("a: "))
     b = int(input("b: "))
     result = divide(a, b)
-except ZeroDivisionError as e:
-    print(f"Error")
+except ZeroDivisionError:
+    print(f"Division by zero error")
 def logarithm(a, b):
     if b <= 0 or a <= 1:
         raise ValueError
     return math.log(b, a)
+try:
+    a = int(input("a: "))
+    b = int(input("b: "))
+    result = logarithm(a, b)
+except ValueError:
+    print(f"Value Error")
 def exponent(a, b):
     return a**b
 
