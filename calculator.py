7ace940541f09905bb3b23c34263893dd87f3339
shivs <shivscs03@gmail.com>
2025-04-14 09:42:55 -0400

    added all functions for calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -9,6 +11,31 @@ def add(a, b):
     return a + b
 def sub(a, b):
     return b-a
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    try:
+        if a!= 0:
+            return b/a
+        else:
+            raise ZeroDivisionError
+    except ZeroDivisionError:
+        print(f"Error: Division by zero")
+
+
+def logarithm(a,b):
+    try:
+        if b<=0 and a<=1:
+            raise ValueError
+        else:
+            return math.log(b,a)
+    except ValueError:
+        print("Error: Negative/0/1 base or Negative/0 argument")
+def exp(a,b):
+    return a**b
+
+
 
 
 
