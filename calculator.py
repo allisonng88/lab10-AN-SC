dc3aa864284db09e865ea9abbb1a62a4ecf961f4
allisonng88 <alli.ng5238@gmail.com>
2025-04-14 03:18:37 -0400

    Added add function

--- calculator.py
+++ calculator.py
@@ -5,10 +5,26 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
+import math
+def add(a, b):
     return a + b
 def sub(a, b):
     return b-a
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+def logarithm(a, b):
+    if b <= 0 or a <= 1:
+        raise ValueError
+    return math.log(b, a)
+def exponent(a, b):
+    return a**b
+
+
+
 
 
 
